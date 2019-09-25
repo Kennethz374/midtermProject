227c439078076a877328ec41b8a7d83744f4d5f1
vagrant <Kennethzhang374@gmail.com>
2019-09-25 21:13:58 +0000

    cheking routes

--- routes/results.js
+++ routes/results.js
@@ -4,19 +4,16 @@ const router  = express.Router();
 module.exports = (dataHelpers) => {
 //For randomizing after the voting is completed
 router.post("/", (req, res) => {
-  console.log('route');
   res.redirect('/results');
 })
 
 router.get("/:poll_string", (req, res) => {
-  console.log('PARAMS',req.params)
   dataHelpers.getRankings(req.params.poll_string)
   .then((data) => {
     console.log('DATA', data);
     dataHelpers.getTotalRanking(req.params.poll_string)
     .then ((data1) => {
       var t = {rankings:data,totalrankings: data1};
-      console.log("ROHIT ",t);
       res.render("results");
     })
 
