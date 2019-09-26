5b784564a2560a86b7c1b7740c33fdc6ca0e5b0d
kirillradaev <cyrilrdv@gmail.com>
2019-09-26 19:02:55 +0000

    fixing conflicts and routes resolution:

--- routes/results.js
+++ routes/results.js
@@ -5,9 +5,54 @@ router.use(cookieParser())
 
 module.exports = (dataHelpers) => {
 
-router.get("/:poll_string", (req, res) => {
-  res.render('results');
-});
+
+  router.get("/:poll_string", (req, res) => {
+    console.log('!!!!HERE!!!!!!!')
+    const results = {};
+    const rankPoints = {
+      1: 3,
+      2: 2,
+      3: 1
+    }
+
+    dataHelpers.getResults(req.params.poll_string)
+    .then((result) => {
+      for(let res of result) {
+        let id = res.option_id;
+        if(results[id]) {
+          results[id].points += rankPoints[res.ranking] || 0;
+        } else {
+          results[id] = {
+            name: res.name,
+            ranking: res.ranking,
+            food: res.food,
+            description: res.description,
+            points: rankPoints[res.ranking]
+          }
+        }
+      }
+
+      let sum = 0;
+      for (let i in results) {
+        if(results[i].points) {
+          sum += results[i].points;
+        }
+      }
+
+      const percentage = function (results) {
+        for(let i in results) {
+          if(results[i].points) {
+            return (Math.round((results[i].points/sum) * 100) + '%');
+          }
+        }
+      }
+
+      console.log(percentage());
+
+
+      res.render("results", {results: Object.values(results)});
+    })
+  });
 
 //For randomizing after the voting is completed
 router.post("/", (req, res) => {
@@ -20,6 +65,7 @@ router.delete("/", (res, req) => {
 });
 
 router.post("/:poll_string", (req, res) => {
+  console.log("HELLOOWORLD")
   dataHelpers.createUser("Kenneth0000")
   .then((data)=>{
     console.log("This is the data ID  ", data.id);
